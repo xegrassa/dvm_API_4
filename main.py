50ba68b72f4f239eed4e2509d247b8d79c455bb9
egrassa <bacek90@mail.ru>
2022-03-20 23:26:26 +0500

    Добавлено создание папки для изображений если ее нет

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
+from pathlib import Path
+
 import requests
 
 URL = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
 
 def main():
+    Path("./images").mkdir(exist_ok=True)
+
     response = requests.get(URL)
 
-    with open('imgs/hubble.jpeg', 'wb') as f:
+    with open('images/hubble.jpeg', 'wb') as f:
         f.write(response.content)
 
 
