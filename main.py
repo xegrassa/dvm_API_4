20de68bdcdcbfcb4f93e2370ef58cd5790c0549b
egrassa <bacek90@mail.ru>
2022-03-20 23:33:44 +0500

    Скачивание картинки вынесено в функцию

--- main.py
+++ main.py
@@ -5,13 +5,17 @@ import requests
 URL = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 
 
+def download_image(url, path):
+    response = requests.get(URL)
+    response.raise_for_status()
+    with open(path, 'wb') as f:
+        f.write(response.content)
+
+
 def main():
     Path("./images").mkdir(exist_ok=True)
 
-    response = requests.get(URL)
-
-    with open('images/hubble.jpeg', 'wb') as f:
-        f.write(response.content)
+    download_image(URL, 'images/hubble.jpeg')
 
 
 if __name__ == '__main__':
