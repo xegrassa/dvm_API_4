e04fff80bae65f738685e4af93c843562c800b94
egrassa <bacek90@mail.ru>
2022-03-21 00:40:46 +0500

    Получение ссылок на картинки запуска ракет SpaceX

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import requests
 
 URL = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
+URL_2 = 'https://api.spacexdata.com/v3/launches/13'
 
 
 def download_image(url, path):
@@ -16,6 +17,11 @@ def main():
     Path("./images").mkdir(exist_ok=True)
 
     download_image(URL, 'images/hubble.jpeg')
+
+    response = requests.get(URL_2)
+    response.raise_for_status()
+    for link in response.json()['links']['flickr_images']:
+        print(link)
 
 
 if __name__ == '__main__':
