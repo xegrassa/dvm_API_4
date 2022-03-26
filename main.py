44029abf0f35dff3fd013f5513745a840092c454
egrassa <bacek90@mail.ru>
2022-03-26 18:13:07 +0500

    Добавлена функция скачивания нескольких фотографий с NASA EPIC

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os.path
 from pathlib import Path
 from urllib.parse import urlparse, unquote
-
+from datetime import datetime
 import requests
 from dotenv import load_dotenv
 
@@ -34,7 +34,8 @@ def fetch_spacex_last_launch():
         download_image(link, f'images/spacex{num}{ext}')
 
 
-def fetch_nasa():
+def fetch_nasa_apod():
+    """Скачивает ~30 изображений дня."""
     token = os.getenv('NASA_TOKEN')
     url = f'https://api.nasa.gov/planetary/apod?count=30&api_key={token}'
     response = requests.get(url)
@@ -45,12 +46,27 @@ def fetch_nasa():
         download_image(link, f'images/nasa{num}{ext}')
 
 
+def fetch_nasa_epic():
+    token = os.getenv('NASA_TOKEN')
+    url = f'https://api.nasa.gov/EPIC/api/natural/images?api_key={token}'
+    response = requests.get(url)
+    response.raise_for_status()
+    images_info = [(image['image'], datetime.fromisoformat(image['date'])) for image in response.json()]
+
+    for num, image_info in enumerate(images_info[:10], start=1):
+        name, date = image_info
+        year, month, day = date.year, date.month, date.day
+        link = f'https://api.nasa.gov/EPIC/archive/natural/{year}/{month:0=2}/{day:0=2}/png/{name}.png?api_key={token}'
+        download_image(link, f'images/nasa_epic{num}.png')
+
+
 def main():
     load_dotenv()
     Path("./images").mkdir(exist_ok=True)
 
-    fetch_spacex_last_launch()
-    fetch_nasa()
+    # fetch_spacex_last_launch()
+    # fetch_nasa_apod()
+    fetch_nasa_epic()
 
 
 if __name__ == '__main__':
