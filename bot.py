bbd96e097fd8d152e14fc86298b30b7b8d96af06
Egrassa <bacek90@mail.ru>
2022-03-27 17:37:36 +0500

    Publish photo to Telegram channel.

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ def main():
     bot = telegram.Bot(token=token)
     print(bot.get_me())
     bot.send_message(chat_id='@space_images_dvmn', text="Сообщение для проверки")
+    bot.send_photo(chat_id='@space_images_dvmn', photo=open('images/nasa_epic1.png', 'rb'))
 
 
 if __name__ == '__main__':
